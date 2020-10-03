9f4deef537664cebeebbfaceec0b2bd571d084a5
Prerit Goel <preritgoel123@gmail.com>
2020-10-03 16:23:28 +0530

    Dividing numbers

---
+++ Divise.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x, y;
+    cout<<" Enter first number: ";
+    cin>>x;
+    cout<<" Enter second number: ";
+    if(y != 0)
+    cout<<endl<<" The quotient is "<<x/y;
+    else cout<<endl<<" Can't divide by zero";
+    return 0;
+
+}
